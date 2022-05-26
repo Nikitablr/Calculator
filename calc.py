848e96d94ca324a796f11169d2409c232e5f3b88
nikrud <89258843+nikrud@users.noreply.github.com>
2022-05-26 17:38:17 +0300

    Improve

--- calc.py
+++ calc.py
@@ -1,28 +1,34 @@
+print('Этот калькулятор умеет - Складывать, Вычитать, Умножать, Делить, Возводить в степень.')
+print("Let's try!!!")
+
 def calc():
-    val_1 = int(input('Введите число: '))
-    action = input('Выберете действие: ')
-    val_2 = int(input('Введите число: '))
-    if action == '+':
-        summ = int(val_1 + val_2)
-        print('Результат: ' + str(summ))
-    elif action == '*':
-        mult = int(val_1 * val_2)
-        print('Результат: ' + str(mult))
-    elif action == '/':
-        try:
-            div = int(val_1 / val_2)
-            print('Результат: ' + str(div))
-        except ZeroDivisionError:
-            if val_2 == 0:
-                print('На ноль делить нельзя!')
-    elif action == '-':
-        sub = int(val_1 - val_2)
-        print('Результат: ' + str(sub))
-    elif action == '^':
-        degree = val_1 ** val_2
-        print('Результат: ' + str(degree))
-    else:
-        print('Такой символ мне не знаком:((')
+    try:
+        val_1 = int(input('Введите число: '))
+        action = input('Выберете действие: ')
+        val_2 = input('Введите число: ')
+        if action == '+':
+            summ = int(val_1 + val_2)
+            print('Результат: ' + str(summ))
+        elif action == '*':
+            mult = int(val_1 * val_2)
+            print('Результат: ' + str(mult))
+        elif action == '/':
+            try:
+                div = int(val_1 / val_2)
+                print('Результат: ' + str(div))
+            except ZeroDivisionError:
+                if val_2 == 0:
+                    print('На ноль делить нельзя!')
+        elif action == '-':
+            sub = int(val_1 - val_2)
+            print('Результат: ' + str(sub))
+        elif action == '^':
+            degree = val_1 ** val_2
+            print('Результат: ' + str(degree))
+        elif action != '+' or action != '*' or action != '/' or action != '-' or action != '^':
+            print('Я умею: Складывать, Вычитать, Умножать, Делить, Возводить в степень! Выбери другое действие!')
+    except ValueError:
+            print('Такой символ мне не знаком:((')
 
 calc()
 
